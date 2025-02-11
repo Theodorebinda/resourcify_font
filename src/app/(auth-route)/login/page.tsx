1d7e1be83cc8e266854b3bb673d8b63bc73c7ac0
theodorebinda <theodorebinda@gmail.com>
2025-02-11 19:17:10 +0100

    🎨 feat: Refine login page text styling and layout

--- src/app/(auth-route)/login/page.tsx
+++ src/app/(auth-route)/login/page.tsx
@@ -7,16 +7,19 @@ const LoginPage = () => {
       <div className="md:w-[40%] md:h-[24rem] flex flex-col justify-start items-start gap-8   ">
         <h3 className="font-bold text-5xl ">Resourcify</h3>
         <div className="flex flex-col gap-4">
-          <span>
-            Crée des articles qui résonnent, découvre des livres qui marquent,
-            et rejoins une communauté qui partage tes passions.
+          <span className="text-3xl text-gray-100">
+            Crée des articles, découvre des livres, et rejoins une communauté.
           </span>
-          <span>
+          <span className="text-xl">
             {" 💡 Trouve ce qui t’inspire et partage ce qui te passionne."}
           </span>
-          <span>{"📚 Ne lis plus seul. Une communaute t'attend."}</span>
-          <span>{"🖋️ Ton savoir peut éclairer d'autres esprits."}</span>
-          <span>
+          <span className="text-xl">
+            {"📚 Ne lis plus seul. Une communaute t'attend."}
+          </span>
+          <span className="text-xl">
+            {"🖋️ Ton savoir peut éclairer d'autres esprits."}
+          </span>
+          <span className="text-xl">
             {
               "Un lecteur vit mille vies avant de mourir. Celui qui ne lit pas n’en vit qu’une"
             }
